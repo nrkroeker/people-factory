5a4f6c5d894b55ae55ec22a1b7d0ace288eabdb7
nrkroeker <nathaliekroeker@gmail.com>
2017-06-05 14:22:17 -0400

    Add bio paragraph

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ function handleSubmit(ev) {
   // Value gets the input value as opposed to the HTML element
   const name = f.personName.value
   const age = f.personAge.value
-
-  const title = name + ", " + age
+  const title = name + ', ' + age
+  const color = f.textColor.value
 
   document.querySelector('h1').textContent = title
+
+  document.querySelector('#personInfo').textContent = name + ' is ' + age + ' years old. Her favorite color is ' + color + '!'
+
+  document.style.color = color
+
 }
 
 personForm.addEventListener('submit', handleSubmit)
